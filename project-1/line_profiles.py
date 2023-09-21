1f53b2cacf35562575d55b39be26eff964141d31
JohanCarlsen <johan_carlsen@hotmail.com>
2023-09-21 13:22:09 +0200

    Cleaning up

--- project-1/line_profiles.py
+++ project-1/line_profiles.py
@@ -8,73 +8,120 @@ plt.rcParams.update({
     'lines.linewidth': 1
 })
 
-def gaussian_profile(frequency, mult_peak=False, baseline=0.1, slope=0.01, scale=100):
-    continuum = slope * frequency + baseline
+def gaussian_profile(frequency, mult_peaks=False, baseline=0.1, slope=0.01, scale=100):
+    '''
+    Compute a Gaussian profile as function of frequency, on the form
 
-    a, c = (1, 1) if not mult_peak else (3, 0.2)
+        f(x) = a * exp(-x^2 / c) + continuum
 
-    exp1 = a * (np.exp(-frequency**2 / c) + continuum) / scale
-    exp2 = 0.3 * (np.exp(-(frequency - 2)**2 / 0.05) + continuum) / scale
-    exp3 = 0.1 * (np.exp(-(frequency - 3)**2 / 0.1) + continuum) / scale
-    mult = exp1 + exp2 + exp3 
+    Parameters:
+    -----------
+    frequency : array_like 
+        Array containing the frequencies.
 
-    return exp1 if not mult_peak else mult
+    mult_peaks : bool, default=False 
+        If set to True, a Gaussian profile with 3 peaks will be returned.
 
-def make_panels(profile_func='gaussian', source_func='exp', mult_mu=False):
+    baseline : float, default=0.1
+        The continuum is considered to be on the form y = a + bx, where a is the baseline.
 
-    figname = source_func
-    figname += 'mult.png' if mult_mu else '.png'
+    slope : float, default=0.01
+        Slope of the continuum (see baseline).
 
+    scale : int or float, default=100
+        The returned profile will be scaled down with this value, ie. if G is the Gaussian, the 
+        returned value is G/scale.
+
+    Returns:
+    --------
+    array_like
+        The Gaussian profile with 1 or 3 peaks plus the baseline.
+    '''
+    cont = baseline + slope * frequency # Continuum
+
+    # Selecting a, c depending on the number of peaks
+    a, c = (1, 1) if not mult_peaks else (3, 0.2)
+
+    exp1 = a * (np.exp(-frequency**2 / c) + cont) / scale
+    exp2 = 0.3 * (np.exp(-(frequency - 2)**2 / 0.05) + cont) / scale
+    exp3 = 0.1 * (np.exp(-(frequency - 3)**2 / 0.1) + cont) / scale
+    
+    profile = exp1 + exp2 + exp3 if mult_peaks else exp1
+
+    return profile   
+
+def make_panels(profile_func='gaussian', source_func='gaussian', mult_mu=False):
+    '''
+    Create four panels for visualising the extinction, optical depth, intensity, and source function.
+
+    Parameters:
+    -----------
+    profile_func : callable or tuple, optional
+        The function describing the extinction profile. User can provide their own if they want.
+        Default is the `gaussian_profile`. If a `tuple`, the elements are (bool, float, float, float),
+        representing the arguments of the `gaussian_profile`.
+
+    source_func : string, optional 
+        Form of the source function. The two forms are:
+
+            * 'gaussian' (default): Gaussian function on the form S(x) = exp[-(x - 500)^2 / (1.5*10^5)] + 0.65
+            * 'linear': Linear function on the form S(x) = 2x / 100 + 0.65
+
+    mult_mu : bool, default=False
+        If True, the intensity panel will display two curves, one for mu = 1, and one for mu = 0.2
+    '''
+    # Number of frequency elements.
     n = 101
 
+    # Frequency from -5 to 5.
     freq = np.linspace(-5, 5, n)
+
+    # Optical depth from 1 to 10^4 with 200 elements.
+    # Height z is the negative height plus the highest depth.
     depth = np.logspace(0, 4, 200)
     z = -depth + depth.max()
 
     if callable(profile_func):
-        profile = profile_func(freq)
-    
+        profile = profile_func
+
     else:
-        if isinstance(profile_func, str):
+        if profile_func == 'gaussian':
             profile = gaussian_profile(freq)
-        
+
         elif isinstance(profile_func, tuple):
-            mult_peak, baseline, slope, scale = profile_func
+            mult_peaks, baseline, slope, scale = profile_func
             profile = gaussian_profile(
                 freq,
-                mult_peak=mult_peak,
+                mult_peaks=mult_peaks,
                 baseline=baseline,
                 slope=slope,
                 scale=scale
             )
-    
-    alpha = np.tile(profile, (len(z), 1))
 
-    tau = cumtrapz(alpha, -z, axis=0, initial=1e-3)
-    tau_z0 = np.argwhere(tau[:, 0] >= 1)[0]
+    # Extinction and optical depth
+    alpha = np.tile(profile, (len(z), 1))
+    tau   = cumtrapz(alpha, -z, axis=0, initial=1e-3)
+
+    # Get the indicies where tau = 1
+    tau_z0    = np.argwhere(tau[:, 0] >= 1)[0]
     tau_zhalf = np.argwhere(tau[:, 50] >= 1)[0]
 
-    exp = np.exp(-(depth - 500)**2 / 1.5e5) + 0.65
-    exp2 = np.exp((depth - 500)**2 / 1e9) - 0.65
-    linear = depth / 100 * 2 + 0.65
+    # Source functions
+    gauss = np.exp(-(depth - 500)**2 / 1.5e5) + 0.65
+    lin   = 2 * depth / 100 + 0.65
 
     S_func = {
-        'exp': exp,
-        'exp2': exp2,
-        'linear': linear
+        'gaussian': gauss,
+        'linear'  : lin
     }
-
-    try: 
-        S = S_func[source_func]
     
-    except KeyError:
-        print('The key ' + source_func + ' is not valid. Possible keys are:\n')
-        [print(key) for key in S_func]
-        exit()
+    S = S_func[source_func]
 
+    # Intensity
     I = np.trapz(S[:, np.newaxis] * np.exp(-tau), tau, axis=0)
-    
-    fig, axes = plt.subplots(2, 2, figsize=(8, 6))
+
+    # Create the figure 
+    fig, axes = plt.subplots(2, 2)
     ax1, ax2, ax3, ax4 = axes.flatten()
 
     ax1.set_title(r'Extinction $\alpha(\nu)$')
@@ -106,10 +153,13 @@ def make_panels(profile_func='gaussian', source_func='exp', mult_mu=False):
     ax4.plot(z[tau_zhalf], S[tau_zhalf], 'ro')
 
     fig.tight_layout()
+    figname = source_func + 'mult' if mult_mu else source_func
     fig.savefig('figures/' + figname, bbox_inches='tight')
 
-make_panels()
-make_panels(profile_func=(True, 0.2, 0, 1000), source_func='linear')
-make_panels(profile_func=(False, 0.2, 0, 1000), source_func='linear', mult_mu=True)
+if __name__ == '__main__':
 
-plt.show()
+    make_panels()
+    make_panels(profile_func=(True, 0.2, 0, 1000), source_func='linear')
+    make_panels(profile_func=(False, 0.2, 0, 1000), source_func='linear', mult_mu=True)
+
+    plt.show()
